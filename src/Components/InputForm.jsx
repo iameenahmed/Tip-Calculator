7eeb17fce6f3bdafdf3ec69123e470ca7064646c
Ameen A <abulhashim351@gmail.com>
2024-04-05 07:16:20 +0500

    Refactor InputForm component to handle input validation and display errors

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,53 +1,41 @@
+import { PeopleCountInput } from "./PeopleCountInput";
+import { BillInput } from "./BillInput";
 import TipInput from "./TipInput";
-import dollarIcon from "../assets/icon-dollar.svg";
-import personIcon from "../assets/icon-person.svg";
+import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
-const InputForm = ({ setInputState }) => {
+const InputForm = ({ setInputState, isResetBtnClicked }) => {
+  const [error, setError] = useState({});
+
   function handleInput(e) {
     const { name, value } = e.target;
+    const regex = /^[1-9]*\.?[1-9]*$/;
+    if (!regex.test(value)) {
+      setError({ ...error, [name]: "Invalid Input" });
+      return;
+    }
+    setError({});
     setInputState((prevState) => ({ ...prevState, [name]: value }));
   }
 
   return (
     <div className="text-VeryDarkCyan md:basis-1/2 md:p-[0.875rem]">
       <form>
-        <label htmlFor="billAmount">Bill</label>
-        <div className="relative">
-          <input
-            type="text"
-            name="billAmount"
-            id="bill"
-            placeholder="0"
-            onChange={handleInput}
-            className="mt-2 block w-full rounded-md bg-VeryLightGrayishCyan px-4 py-3 text-right placeholder:text-VeryDarkCyan"
-          />
-          <img
-            src={dollarIcon}
-            alt="dollar icon"
-            className="absolute left-4 top-1/2 -translate-y-1/2 transform"
-          />
-        </div>
-
-        <TipInput handleInput={handleInput} />
-
-        <label htmlFor="number-of-people">Number of People</label>
-
-        <div className="relative">
-          <input
-            type="text"
-            name="peopleCount"
-            id="number-of-people"
-            placeholder="0"
-            onChange={handleInput}
-            className="mt-2 block w-full rounded-md bg-VeryLightGrayishCyan px-4 py-3 text-right placeholder:text-VeryDarkCyan"
-          />
-          <img
-            src={personIcon}
-            alt="person icon"
-            className="absolute left-4 top-1/2 -translate-y-1/2 transform"
-          />
-        </div>
+        <BillInput
+          handleInput={handleInput}
+          error={error.billAmount}
+          isResetBtnClicked={isResetBtnClicked}
+        />
+        <TipInput
+          handleInput={handleInput}
+          error={error.tipPercent}
+          isResetBtnClicked={isResetBtnClicked}
+        />
+        <PeopleCountInput
+          handleInput={handleInput}
+          error={error.peopleCount}
+          isResetBtnClicked={isResetBtnClicked}
+        />
       </form>
     </div>
   );
