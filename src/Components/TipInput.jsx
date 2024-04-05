86cc680174db7a6ba23b3c4373c6e6e234ac1c9b
Ameen A <abulhashim351@gmail.com>
2024-04-05 07:16:03 +0500

    Refactor TipInput component to handle reset button click and display errors

--- src/Components/TipInput.jsx
+++ src/Components/TipInput.jsx
@@ -1,23 +1,48 @@
+import { useEffect, useRef, useState } from "react";
+
 // eslint-disable-next-line react/prop-types
-const TipInput = ({ handleInput }) => {
+const TipInput = ({ handleInput, error, isResetBtnClicked }) => {
+  const [selectedTip, setSelectedTip] = useState();
+
   const tipOptions = [5, 10, 15, 25, 50];
 
+  const inputRef = useRef(null);
+  const radioRef = useRef(null);
+  isResetBtnClicked && (inputRef.current.value = "");
+  isResetBtnClicked && (radioRef.current.checked = false);
+  useEffect(() => {
+    setSelectedTip("");
+  }, [isResetBtnClicked]);
+
+  const handleRadioChange = (e) => {
+    handleInput(e);
+    setSelectedTip(e.target.value);
+  };
+
   return (
-    <div className="my-8">
-      <h2 className="mb-4">Select tip %</h2>
+    <fieldset className="my-8">
+      <div className="flex items-center justify-between">
+        <legend className="mb-4">Select tip %</legend>
+        {error && <span className="block text-sm text-red-500">{error}</span>}
+      </div>
       <div className="grid grid-cols-2 grid-rows-3 gap-4 font-bold md:grid-cols-3 md:grid-rows-2">
         {tipOptions.map((tipOption, index) => {
           return (
             <label
               key={index}
-              className="block cursor-pointer rounded-md bg-VeryDarkCyan py-3 text-center text-White hover:bg-StrongCyan"
+              className={`block cursor-pointer rounded-md py-3 text-center text-White transition-colors duration-300 md:text-2xl ${
+                selectedTip === tipOption.toString()
+                  ? "bg-StrongCyan"
+                  : "bg-VeryDarkCyan hover:bg-StrongCyan"
+              }`}
             >
               <input
                 type="radio"
+                ref={radioRef}
                 name="tipPercent"
                 value={tipOption}
                 className="hidden"
-                onChange={handleInput}
+                onChange={handleRadioChange}
               />
               {`${tipOption}%`}
             </label>
@@ -26,13 +51,16 @@ const TipInput = ({ handleInput }) => {
         <input
           type="text"
           name="tipPercent"
-          id="tip-percent"
+          id="tipPercent"
+          ref={inputRef}
           placeholder="Custom"
           onChange={handleInput}
-          className="rounded-md bg-VeryLightGrayishCyan px-3.5 py-3 text-right text-DarkGrayishCyan placeholder:text-VeryDarkCyan"
+          className={`rounded-md bg-VeryLightGrayishCyan px-3.5 py-3 text-right text-DarkGrayishCyan outline-StrongCyan placeholder:text-VeryDarkCyan md:text-2xl ${
+            error ? "outline-red-500" : "outline-StrongCyan"
+          }`}
         />
       </div>
-    </div>
+    </fieldset>
   );
 };
 
